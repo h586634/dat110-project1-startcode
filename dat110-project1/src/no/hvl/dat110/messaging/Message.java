335af934f5b40ed33cbf98d2cae51d822ca1968f
Eier <Eier@DESKTOP-PJA8TRP>
2021-02-04 09:35:05 +0100

    Ferdig med Message

--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -9,7 +9,12 @@ public class Message {
 	private byte[] payload;
 
 	public Message(byte[] payload) {
-		this.payload = payload; // TODO: check for length within boundary
+
+		if (payload.length <= 127) {
+			this.payload = payload;
+		} else {
+			payload = null;
+		}
 	}
 
 	public Message() {
@@ -17,31 +22,28 @@ public class Message {
 	}
 
 	public byte[] getData() {
-		return this.payload; 
+		return this.payload;
 	}
 
 	public byte[] encapsulate() {
-		
+
 		byte[] encoded = null;
-		
-		// TODO
-		// encapulate/encode the payload of this message in the
-		// encoded byte array according to message format
-		
-		if (true)
-		   throw new UnsupportedOperationException(TODO.method());
+		encoded = new byte[128];
+		encoded[0] = (byte) payload.length;
+		for (int i = 0; i <= payload.length; i++) {
+			encoded[i] = payload[i - 1];
+		}
 
 		return encoded;
-		
+
 	}
 
 	public void decapsulate(byte[] received) {
 
-		// TODO
-		// decapsulate the data contained in the received byte array and store it 
-		// in the payload of this message
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		payload = new byte[received[0]];
+		for (int i = 1; i <= received[0]; i++) {
+			payload[i - 1] = received[i];
+		}
 	}
+
 }
