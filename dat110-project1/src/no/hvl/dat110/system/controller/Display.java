6be0f65ae2a6e52ae858ea16f554848b4f3c5540
Eier <Eier@158.37.225.233>
2021-02-05 17:10:30 +0100

    Ferdig Controller Display

--- dat110-project1/src/no/hvl/dat110/system/controller/Display.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Display.java
@@ -9,9 +9,11 @@ public class Display extends RPCStub {
 
 	public void write(String message) {
 
-		// TODO
-		// implement marshalling, call and unmarshalling for write RPC method
+		byte[] request = RPCUtils.marshallString(RPCID, message);
 
-		throw new UnsupportedOperationException(TODO.method());
+		byte[] response = rpcclient.call(request);
+
+		RPCUtils.unmarshallString(response);
+
 	}
 }
