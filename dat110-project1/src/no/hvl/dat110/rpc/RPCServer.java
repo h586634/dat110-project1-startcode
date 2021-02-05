0abcb7ab46a9facfa56beb76ca5d48d10c08a03b
PaTrond <trond@hauge.app>
2021-02-05 12:37:57 +0100

    RPCServer - done?

--- dat110-project1/src/no/hvl/dat110/rpc/RPCServer.java
+++ dat110-project1/src/no/hvl/dat110/rpc/RPCServer.java
@@ -11,61 +11,60 @@ public class RPCServer {
 
 	private MessagingServer msgserver;
 	private Connection connection;
-	
+
 	// hashmap to register RPC methods which are required to implement RPCImpl
-	
-	private HashMap<Integer,RPCImpl> services;
-	
+
+	private HashMap<Integer, RPCImpl> services;
+
 	public RPCServer(int port) {
-		
+
 		this.msgserver = new MessagingServer(port);
-		this.services = new HashMap<Integer,RPCImpl>();
-		
+		this.services = new HashMap<Integer, RPCImpl>();
+
 		// the stop RPC methods is built into the server
-		services.put((int)RPCCommon.RPIDSTOP,new RPCServerStopImpl());
+		services.put((int) RPCCommon.RPIDSTOP, new RPCServerStopImpl());
 	}
-	
+
 	public void run() {
-		
+
 		System.out.println("RPC SERVER RUN - Services: " + services.size());
-		
-		connection = msgserver.accept(); 
-		
+
+		connection = msgserver.accept();
+
 		System.out.println("RPC SERVER ACCEPTED");
-		
+
 		boolean stop = false;
-		
+
 		while (!stop) {
-	    
-		   int rpcid;
-		   
-		   //test
-		   
-		   // TODO
-		   // - receive message containing RPC request
-		   // - find the identifier for the RPC methods to invoke
-		   // - lookup the method to be invoked
-		   // - invoke the method
-		   // - send back message containing RPC reply
-			
-		   if (true) {
-			   throw new UnsupportedOperationException(TODO.method());
-		   }
-		   
-		   if (rpcid == RPCCommon.RPIDSTOP) {
-			   stop = true;
-		   }
+
+			Message msg = connection.receive();
+
+			byte[] rData = msg.getData();
+
+			int rpcid = rData[0];
+
+			if (rpcid == RPCCommon.RPIDSTOP) {
+				stop = true;
+				continue;
+			}
+
+			byte[] sData = services.get(rpcid).invoke(rData);
+
+			try {
+				connection.send(new Message(sData));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-	
 	}
-	
+
 	public void register(int rpcid, RPCImpl impl) {
 		services.put(rpcid, impl);
 	}
-	
+
 	public void stop() {
 		connection.close();
 		msgserver.stop();
-		
+
 	}
 }
