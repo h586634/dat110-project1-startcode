e56f37079d4bec23858ff5f57b3a756f4cd26a6a
Eier <Eier@158.37.225.233>
2021-02-05 17:02:47 +0100

    Ferdig med Controller Common

--- dat110-project1/src/no/hvl/dat110/system/controller/Controller.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Controller.java
@@ -4,52 +4,62 @@ import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.RPCClient;
 import no.hvl.dat110.rpc.RPCServerStopStub;
 
-public class Controller  {
-	
+public class Controller {
+
 	private static int N = 5;
-	
-	public static void main (String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		Display display;
 		Sensor sensor;
-		
-		RPCClient displayclient,sensorclient;
-		
+
+		RPCClient displayclient, sensorclient;
+
 		System.out.println("Controller starting ...");
-				
+
 		RPCServerStopStub stopdisplay = new RPCServerStopStub();
 		RPCServerStopStub stopsensor = new RPCServerStopStub();
-		
-		displayclient = new RPCClient(Common.DISPLAYHOST,Common.DISPLAYPORT);
-		sensorclient = new RPCClient(Common.SENSORHOST,Common.SENSORPORT);
-		
-		// TODO
+
+		displayclient = new RPCClient(Common.DISPLAYHOST, Common.DISPLAYPORT);
+		sensorclient = new RPCClient(Common.SENSORHOST, Common.SENSORPORT);
+
 		// connect to sensor and display RPC servers
+		displayclient.connect();
+		sensorclient.connect();
+
 		// create local display and sensor objects
+		display = new Display();
+		sensor = new Sensor();
+
 		// register display and sensor objects in the RPC layer
-		
+		displayclient.register(display);
+		sensorclient.register(sensor);
+
 		if (true) {
 			throw new UnsupportedOperationException(TODO.method());
 		}
-		
+
 		// register stop methods in the RPC layer
 		displayclient.register(stopdisplay);
 		sensorclient.register(stopsensor);
-		
+
 		// TODO:
 		// loop while reading from sensor and write to display via RPC
 		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-			}
-		
-		stopdisplay.stop();
-		stopsensor.stop();
-	
-		displayclient.disconnect();
-		sensorclient.disconnect();
-		
-		System.out.println("Controller stopping ...");
-		
+		for(int i=0; i<N; i++) {
+			int temp = sensor.read();
+			String lesTemp = String.valueOf(temp);
+			display.write(lesTemp);
+			
+		}
+
+			stopdisplay.stop();
+			stopsensor.stop();
+
+			displayclient.disconnect();
+			sensorclient.disconnect();
+
+			System.out.println("Controller stopping ...");
+
+		}
 	}
-}
