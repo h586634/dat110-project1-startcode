727cd843e160671bb51e922a6efac2a7df1e328f
PaTrond <trond@hauge.app>
2021-02-05 17:28:55 +0100

    Sensor.java ferdig

--- dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
@@ -1,6 +1,5 @@
 package no.hvl.dat110.system.controller;
 
-import no.hvl.dat110.TODO;
 import no.hvl.dat110.rpc.*;
 
 public class Sensor extends RPCStub {
@@ -9,16 +8,10 @@ public class Sensor extends RPCStub {
 	
 	public int read() {
 		
-		int temp;
-		
-		// TODO
-		// implement marshalling, call and unmarshalling for read RPC method
-		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
-		
-		return temp;
+		byte[] request = RPCUtils.marshallVoid(RPCID);
+		byte[] response = rpcclient.call(request);
+				
+		return RPCUtils.unmarshallInteger(response);
 	}
 	
 }
